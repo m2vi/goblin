0a4d4834607dbda8011dce8b41f7002460ba3422
m2vi <iuvo@gmx.at>
2022-09-18 22:26:06 +0200

    fix

--- src/pages/api/modpack.ts
+++ src/pages/api/modpack.ts
@@ -1,7 +1,8 @@
+import type { APIContext } from 'astro';
 import fs from 'fs-extra';
 import path from 'path';
 
-export async function get() {
+export async function get({}: APIContext) {
   const buffer = fs.readFileSync(path.join(process.cwd(), 'src', 'data', 'modpack.zip'));
 
   return {
