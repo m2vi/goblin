35ee163e94bf4c87d108fd743b4e7bbb857487ac
m2vi <iuvo@gmx.at>
2022-09-19 19:35:31 +0200

    fix

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,14 +1,7 @@
 const Button = () => {
   return (
     <>
-      <button
-        onClick={(e) => {
-          if (process.env.NODE_ENV === 'development') {
-            e.preventDefault();
-          }
-        }}
-        className='leading-none bg-white text-primary border-0 text-lg font-bold transition-all uppercase rounded-md px-8 py-4'
-      >
+      <button className='leading-none bg-white text-primary border-0 text-lg font-bold transition-all uppercase rounded-md px-8 py-4'>
         download
       </button>
     </>
